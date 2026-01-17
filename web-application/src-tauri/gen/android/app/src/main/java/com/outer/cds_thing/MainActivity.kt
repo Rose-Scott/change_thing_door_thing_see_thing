7dc8910a8f59df25b50f125e1accb5d093a0be17
Outer Cloud <outercloudstudio@gmail.com>
2026-01-16 21:56:53 -0500

    add gen

---
+++ web-application/src-tauri/gen/android/app/src/main/java/com/outer/cds_thing/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.outer.cds_thing
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
