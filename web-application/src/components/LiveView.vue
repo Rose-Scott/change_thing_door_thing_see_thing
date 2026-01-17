47c3a37a66ac1170486f05f9dcaee760572ca949
Outer Cloud <outercloudstudio@gmail.com>
2026-01-17 11:11:55 -0500

    working on ui

---
+++ web-application/src/components/LiveView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const ip = ref('10.142.233.231')
+const streamUrl = computed(() => 'http://' + ip.value + ':81/stream')
+</script>
+
+<template>
+    <div class="center-view">
+        <img :src="streamUrl" />
+    </div>
+</template>
+
+<style scoped>
+img {
+    background: white;
+    width: 100vw;
+}
+
+.center-view {
+    display: flex;
+
+    justify-content: center;
+    align-items: center;
+
+    width: 100vw;
+    height: 100vh;
+}
+</style>
---
+++ web-application/src/components/TopBar.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="top-bar">
+        <p>Live View</p>
+        <p>22%</p>
+    </div>
+</template>
+
+<style scoped>
+p {
+    margin: 0;
+}
+
+.top-bar {
+    display: flex;
+    justify-content: space-between;
+
+    background-image: linear-gradient(#000000ac, #00000000);
+
+    padding: 0.5rem;
+    padding-bottom: 1rem;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: calc(100% - 1rem);
+}
+</style>
