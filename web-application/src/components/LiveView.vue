5365a6992aa1c1f09fa60a3061f918d1cbb78ab3
Outer Cloud <outercloudstudio@gmail.com>
2026-01-18 10:23:31 -0500

    Revert "failed audio"
    This reverts commit 835faf587de43471d4e653fa76ba9595fc542012.

--- web-application/src/components/LiveView.vue
+++ web-application/src/components/LiveView.vue
@@ -6,7 +6,6 @@ import { isPermissionGranted, requestPermission, sendNotification } from '@tauri
 const ip = ref('10.142.233.231')
 const streamUrl = computed(() => 'http://' + ip.value + ':81/stream')
 const audioStreamUrl = computed(() => 'http://' + ip.value + ':82/audio')
-const recordStreamUrl = computed(() => 'http://' + ip.value + ':82/record')
 
 const image = useTemplateRef('image')
 
@@ -40,68 +39,16 @@ onMounted(() => {
     image.value.onerror = onErrorLoadingLiveView
 
     // dealWithAudio()
-
-    setInterval(() => {
-        recordAndPlayback()
-    }, 5000)
 })
 
-async function recordAndPlayback() {
-    console.log('Requesting recording from ESP32...')
-
-    const response = await fetch(recordStreamUrl.value)
-    const audioData = await response.arrayBuffer()
-
-    console.log('Received audio:', audioData.byteLength, 'bytes')
-
-    // Check the data
-    const int16Array = new Int16Array(audioData)
-    console.log('First 10 samples:', int16Array.slice(0, 10))
-    console.log('Min:', Math.min(...int16Array))
-    console.log('Max:', Math.max(...int16Array))
-
-    // If all zeros or all same value, microphone isn't working
-    if (Math.min(...int16Array) === Math.max(...int16Array)) {
-        console.error('No audio data - microphone not working!')
-        return
+function float32ToInt16(float32Array: Float32Array) {
+    const int16Array = new Int16Array(float32Array.length)
+    for (let i = 0; i < float32Array.length; i++) {
+        const s = Math.max(-1, Math.min(1, float32Array[i]))
+        int16Array[i] = s < 0 ? s * 0x8000 : s * 0x7fff
     }
-
-    // Convert Int16 to Float32
-    const float32Array = new Float32Array(int16Array.length)
-    for (let i = 0; i < int16Array.length; i++) {
-        float32Array[i] = int16Array[i] / 32768.0
-    }
-
-    console.log('Float32 range:', Math.min(...float32Array), 'to', Math.max(...float32Array))
-
-    // Play the audio
-    const audioContext = new AudioContext()
-    console.log('AudioContext sample rate:', audioContext.sampleRate)
-
-    // Create buffer with correct sample rate
-    const audioBuffer = audioContext.createBuffer(1, float32Array.length, 16000)
-    audioBuffer.copyToChannel(float32Array, 0)
-
-    const source = audioContext.createBufferSource()
-    source.buffer = audioBuffer
-    source.connect(audioContext.destination)
-
-    console.log('Starting playback...')
-    source.start(0)
-
-    source.onended = () => {
-        console.log('Playback finished')
-    }
+    return int16Array
 }
-
-// function float32ToInt16(float32Array: Float32Array) {
-//     const int16Array = new Int16Array(float32Array.length)
-//     for (let i = 0; i < float32Array.length; i++) {
-//         const s = Math.max(-1, Math.min(1, float32Array[i]))
-//         int16Array[i] = s < 0 ? s * 0x8000 : s * 0x7fff
-//     }
-//     return int16Array
-// }
 
 // let timer = 0
 
